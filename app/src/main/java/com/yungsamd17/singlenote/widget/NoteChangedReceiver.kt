0d3ab5dcf6e7388f7b62be85816ee2c674245fa6
Sam <yungsamd17@users.noreply.github.com>
2026-08-23 12:52:09 +0000

    Import GlanceAppWidget.updateAll extension

--- app/src/main/java/com/yungsamd17/singlenote/widget/NoteChangedReceiver.kt
+++ app/src/main/java/com/yungsamd17/singlenote/widget/NoteChangedReceiver.kt
@@ -7,6 +7,7 @@ import android.service.quicksettings.TileService
 import com.yungsamd17.singlenote.data.ACTION_NOTE_UPDATED
 import com.yungsamd17.singlenote.notify.PinNotification
 import com.yungsamd17.singlenote.tile.NoteTileService
+import androidx.glance.appwidget.updateAll
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
