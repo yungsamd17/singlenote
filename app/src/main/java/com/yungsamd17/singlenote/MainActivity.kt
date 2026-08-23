b4c397c5f7b359a0edd2ceacc5c8a5b20d61bbba
Sam <yungsamd17@users.noreply.github.com>
2026-08-23 13:22:53 +0000

    Restore missing Surface import

--- app/src/main/java/com/yungsamd17/singlenote/MainActivity.kt
+++ app/src/main/java/com/yungsamd17/singlenote/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
