cd1f622bd373441185978b6a5921611ff4626936
Sam <yungsamd17@users.noreply.github.com>
2026-08-24 11:53:43 +0000

    release(app): v0.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.yungsamd17.singlenote"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.2.4"
+        versionCode = 7
+        versionName = "0.2.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
