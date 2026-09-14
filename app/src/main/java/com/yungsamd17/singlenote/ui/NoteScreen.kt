3088d5d298aeb27d9ddfc00b5d06f2360a3bad27
Sam <yungsamd17@users.noreply.github.com>
2026-09-14 04:28:21 +0000

    fix(dev): import WindowInsets.ime for swap threshold

--- app/src/main/java/com/yungsamd17/singlenote/ui/NoteScreen.kt
+++ app/src/main/java/com/yungsamd17/singlenote/ui/NoteScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.foundation.layout.navigationBarsPadding
